a88ca2d4e9a88dc9d82cc1f0b351831b343d7837
skotwind-res <skotwind.res.2@gmail.com>
2021-07-07 20:50:10 +0300

    done task

--- cicdtest/myapp/management/commands/create_data.py
+++ cicdtest/myapp/management/commands/create_data.py
@@ -1,7 +1,8 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 import importlib
 from myapp.models import Specialty, Company, Vacancy
 from datetime import datetime
+from django.db import transaction
 
 
 class Command(BaseCommand):
@@ -10,24 +11,25 @@ class Command(BaseCommand):
 
     def handle(self, *args, source, **options):
         module = importlib.import_module(source[-1])
-        for com in module.companies:
-            Company.objects.create(
-                name=com['title'],
-                logo=com['logo'],
-                description=com['description'],
-                employee_count=com['employee_count']
-            )
-        for spec in module.specialties:
-            Specialty.objects.create(**spec)
-        for vac in module.jobs:
-            Vacancy.objects.create(
-                title=vac['title'],
-                specialty_id=vac['specialty'],
-                company_id=vac['company'],
-                skills=vac['skills'],
-                description=vac['description'],
-                salary_min=vac['salary_from'],
-                salary_max=vac['salary_to'],
-                published_at=datetime.strptime(vac['posted'], "%Y-%m-%d")
-            )
+        with transaction.atomic():
+            for com in module.companies:
+                Company.objects.create(
+                    name=com['title'],
+                    logo=com['logo'],
+                    description=com['description'],
+                    employee_count=com['employee_count']
+                )
+            for spec in module.specialties:
+                Specialty.objects.create(**spec)
+            for vac in module.jobs:
+                Vacancy.objects.create(
+                    title=vac['title'],
+                    specialty_id=vac['specialty'],
+                    company_id=vac['company'],
+                    skills=vac['skills'],
+                    description=vac['description'],
+                    salary_min=vac['salary_from'],
+                    salary_max=vac['salary_to'],
+                    published_at=datetime.strptime(vac['posted'], "%Y-%m-%d")
+                )
 
